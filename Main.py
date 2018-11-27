c2e51b46d0a0a695904fc6955b3f62c51abf3f00
MBacallado <manuelbacallado89@gmail.com>
2018-11-27 11:57:52 +0000

    platform_version modified in Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 from apps.MirApp import MirApp
 
 def test1():
-    mir_app_test = MirApp("Android", "Android", "Xiaomi", "7.1.2", "com.mbacallado.thirdexampleandroid",
+    mir_app_test = MirApp("Android", "Android", "Xiaomi", "8.1.0", "com.mbacallado.thirdexampleandroid",
                               ".activities.MainActivity")
     mir_app_test.run_app("http://127.0.0.1:4723/wd/hub", "Tenerife", "Buenavista del Norte", "Mirador Altos de Baracán")
 
